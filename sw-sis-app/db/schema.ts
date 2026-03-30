9157667ea66932efd2dd6ef8813264d109e0fdb3
Ivan Cadiang <90773604+ZenoxisIV@users.noreply.github.com>
2026-03-30 17:06:30 +0800

    db(schema): add `subjects` table

--- sw-sis-app/db/schema.ts
+++ sw-sis-app/db/schema.ts
@@ -1,4 +1,4 @@
-import { pgTable, uuid, text, timestamp, pgEnum } from 'drizzle-orm/pg-core';
+import { pgTable, uuid, text, timestamp, integer, pgEnum } from 'drizzle-orm/pg-core';
 
 // 1. COURSES
 export const courses = pgTable('courses', {
@@ -10,3 +10,13 @@ export const courses = pgTable('courses', {
   updatedAt: timestamp('updated_at').defaultNow().notNull(),
 });
 
+// 2. SUBJECTS
+export const subjects = pgTable('subjects', {
+  id: uuid('id').primaryKey().defaultRandom(),
+  courseId: uuid('course_id').references(() => courses.id),
+  code: text('code').notNull(),
+  title: text('title').notNull(),
+  units: integer('units').notNull(),
+  createdAt: timestamp('created_at').defaultNow().notNull(),
+  updatedAt: timestamp('updated_at').defaultNow().notNull(),
+});

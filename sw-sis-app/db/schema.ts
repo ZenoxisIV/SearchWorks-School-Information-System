c390cbac5d3f765a11845c66d245ffa421f5affd
Ivan Cadiang <90773604+ZenoxisIV@users.noreply.github.com>
2026-03-30 17:09:08 +0800

    db(schema): add `students` table

--- sw-sis-app/db/schema.ts
+++ sw-sis-app/db/schema.ts
@@ -1,4 +1,4 @@
-import { pgTable, uuid, text, timestamp, integer, pgEnum } from 'drizzle-orm/pg-core';
+import { pgTable, uuid, text, date, timestamp, integer, pgEnum } from 'drizzle-orm/pg-core';
 
 // 1. COURSES
 export const courses = pgTable('courses', {
@@ -17,6 +17,19 @@ export const subjects = pgTable('subjects', {
   code: text('code').notNull(),
   title: text('title').notNull(),
   units: integer('units').notNull(),
+  createdAt: timestamp('created_at').defaultNow().notNull(),
+  updatedAt: timestamp('updated_at').defaultNow().notNull(),
+});
+
+// 3. STUDENTS
+export const students = pgTable('students', {
+  id: uuid('id').primaryKey().defaultRandom(),
+  studentNo: text('student_no').notNull().unique(),
+  firstName: text('first_name').notNull(),
+  lastName: text('last_name').notNull(),
+  email: text('email').unique(),
+  birthDate: date('birth_date').notNull(),
+  courseId: uuid('course_id').references(() => courses.id),
   createdAt: timestamp('created_at').defaultNow().notNull(),
   updatedAt: timestamp('updated_at').defaultNow().notNull(),
 });
